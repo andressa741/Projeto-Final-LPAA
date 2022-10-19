379ea8f1780c5978bc99de69763a3eafab3a3d94
Andressa Freitas De Arruda <dessa931@gmail.com>
2022-10-19 15:20:26 -0300

    Import dos datasets

--- Classificacao.py
+++ Classificacao.py
@@ -4,3 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+# %%
+# Import do dataset
+from sklearn.datasets import load_breast_cancer
+X,y = load_breast_cancer(return_X_y= True, as_frame = True)
--- Regressão.py
+++ Regressão.py
@@ -5,3 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 # %%
+# Import do dataset
+from sklearn.datasets import load_diabetes
+X,y = load_diabetes(return_X_y = True, as_frame = True, scaled = False)
+# %%
