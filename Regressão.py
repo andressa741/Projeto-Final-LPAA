d426c534c36b130919e23ab79c1970dfe6330174
Andressa Freitas De Arruda <dessa931@gmail.com>
2022-10-24 12:01:32 -0300

    Tratamento e preparação dos dados - Regressao

--- Regressão.py
+++ Regressão.py
@@ -9,3 +9,41 @@ import seaborn as sns
 from sklearn.datasets import load_diabetes
 X,y = load_diabetes(return_X_y = True, as_frame = True, scaled = False)
 # %%
+# Tratamento e preparação dos dados
+print(X.head())
+print(load_diabetes().DESCR)
+print(X.info())
+# %%
+# Mudando o tipo da coluna sex para int64
+X['sex'] = X['sex'].astype(np.int64)
+print("Shape X:")
+print(X.shape)
+print("Shape y:")
+print(y.shape)
+print(X.describe())
+ 
+# %%
+sns.pairplot(X)
+# %%
+sns.heatmap(X.corr(),annot = True)
+# %%
+# Verificando outliers
+plt.figure()
+sns.boxplot(x="s1", data = X)
+plt.show()
+plt.figure()
+sns.boxplot(x="s2", data = X)
+plt.show()
+plt.figure()
+sns.boxplot(x="s3", data = X)
+plt.show()
+plt.figure()
+sns.boxplot(x="s4", data = X)
+plt.show()
+plt.figure()
+sns.boxplot(x="s5", data = X)
+plt.show()
+plt.figure()
+sns.boxplot(x="s6", data = X)
+plt.show()
+# %%
